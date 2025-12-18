b55f36233a371fee9ae3f7875015449a5b869403
Murilo Vinicius <Murilo.vinicius.9404@gmail.com>
2025-12-18 19:29:36 -0300

    fix: log_event aceita parametro user

--- logs.py
+++ logs.py
@@ -1,3 +1,13 @@
-def log_event(tipo, msg):
-    with open("audit.log", "a") as f:
-        f.write(f"{tipo} | {msg}\n")
+from datetime import datetime
+import json
+
+def log_event(action, user=None, ip=None, extra=None):
+    log = {
+        "timestamp": datetime.utcnow().isoformat(),
+        "action": action,
+        "user": user,
+        "ip": ip,
+        "extra": extra
+    }
+
+    print(json.dumps(log))
